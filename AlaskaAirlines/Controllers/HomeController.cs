3feb26ceb30a66bdca8bf82178f81125555c23c3
Cameron Bacon <cameronmbacon@gmail.com>
2017-10-25 14:21:10 -0700

    can search flights and sort by price & depart time

--- AlaskaAirlines/Controllers/HomeController.cs
+++ AlaskaAirlines/Controllers/HomeController.cs
@@ -6,18 +6,63 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
 using AlaskaAirlines.Models;
+using AlaskaAirlines.ViewModels;
 using CsvHelper;
 
 namespace AlaskaAirlines.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string From, string To, string SortBy)
         {
-            var csv = new CsvReader(new StreamReader("./Data/airports.csv"));
-            var airports = csv.GetRecords<Airport>();
+            List<Flight> FilteredFlights = new List<Flight>();
 
-            return View(airports.ToList());
+            var csv = new CsvReader(new StreamReader("./Data/airports.csv"));
+            var airports = csv.GetRecords<Airport>().ToList();
+
+            csv = new CsvReader(new StreamReader("./Data/flights.csv"));
+            var flights = csv.GetRecords<Flight>().ToList();
+
+            foreach (var f in flights)
+            {
+                if (f.From == From && f.To == To)
+                {
+                    DateTime departs = DateTime.Parse(f.Departs);
+                    DateTime arrives = DateTime.Parse(f.Arrives);
+
+                    f.Departs = departs.ToString("HH:mm");
+                    f.Arrives = arrives.ToString("HH:mm");
+
+                    FilteredFlights.Add(f);
+                }
+            }
+
+            if (!String.IsNullOrEmpty(SortBy))
+            {
+                switch (SortBy)
+                {
+                    case "Departs":
+                        FilteredFlights.Sort((x, y) => x.Departs.CompareTo(y.Departs));
+                        break;
+                    case "Arrives":
+                        FilteredFlights.Sort((x, y) => x.Arrives.CompareTo(y.Arrives));
+                        break;
+                    case "MainCabin":
+                        FilteredFlights.Sort((x, y) => x.MainCabinPrice.CompareTo(y.MainCabinPrice));
+                        break;
+                    case "FirstClass":
+                        FilteredFlights.Sort((x, y) => x.MainCabinPrice.CompareTo(y.MainCabinPrice));
+                        break;
+                }
+            }
+
+            var FlightVM = new FlightViewModel
+            {
+                Airports = airports,
+                Flights = FilteredFlights
+            };
+
+            return View(FlightVM);
         }
     }
 }
